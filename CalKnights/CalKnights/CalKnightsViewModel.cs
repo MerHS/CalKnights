2ae74e200e2790ca77c80de32064ac37284f371a
MerHS <starvessel@naver.com>
2020-01-31 00:28:11 +0900

    using skiasharp

--- CalKnights/CalKnights/CalKnightsViewModel.cs
+++ CalKnights/CalKnights/CalKnightsViewModel.cs
@@ -10,6 +10,9 @@ using XLabs.Platform.Device;
 using System.Threading.Tasks;
 using Plugin.Media;
 using Plugin.Media.Abstractions;
+using System.Diagnostics;
+using System.IO;
+using SkiaSharp;
 
 namespace CalKnights
 {
@@ -20,68 +23,117 @@ namespace CalKnights
         private readonly ITesseractApi _tesseractApi;
         private readonly IDevice _device;
 
-        private bool _imageLoading = false;
+        private bool _imageWaiting = true;
         private ImageSource _ocrImageSource;
         private Dictionary<string, string> _dicts;
+        private List<Boolean> _isFrameVisible = new List<Boolean> { true, true, true };
+
+        private SKColorFilter _greyFilter = SKColorFilter.CreateColorMatrix(new float[]
+                {
+                    0.21f, 0.72f, 0.07f, 0, 0,
+                    0.21f, 0.72f, 0.07f, 0, 0,
+                    0.21f, 0.72f, 0.07f, 0, 0,
+                    0,     0,     0,     1, 0
+                });
 
         public CalKnightsViewModel()
-        { 
+        {
             _tesseractApi = Resolver.Resolve<ITesseractApi>();
             _device = Resolver.Resolve<IDevice>();
             _dicts = new Dictionary<string, string>
             {
                 { "vanguard", "test" }
             };
-            //DigitCommand = new Command<string>(
-            //    execute: async (string arg) =>
-            //    {
-            //        _takePictureButton.Text = "Working...";
-            //        _takePictureButton.IsEnabled = false;
 
-            //        //TesseractAPI 초기화 (언어 선정)
-            //        if (!_tesseractApi.Initialized)
-            //            await _tesseractApi.Init("eng");
-
-            //        //카메라 촬영 후 byte[] 형식 변수로 받아오기
-            //        var photo = await TakePic();
-            //        if (photo != null)
-            //        {
-            //            var imageBytes = new byte[photo.Source.Length];
-            //            photo.Source.Position = 0;
-            //            photo.Source.Read(imageBytes, 0, (int)photo.Source.Length);
-            //            photo.Source.Position = 0;
-
-            //            //문자 인식 수행
-            //            var tessResult = await _tesseractApi.SetImage(imageBytes);
-
-            //            //촬영이미지, 인식 결과 출력
-            //            if (tessResult)
-            //            {
-            //                _takenImage.Source = ImageSource.FromStream(() => photo.Source);
-            //                _recognizedTextLabel.Text = _tesseractApi.Text;
-            //            }
-            //        }
-
-            //        //버튼 상태 복구
-            //        _takePictureButton.Text = "New scan";
-            //        _takePictureButton.IsEnabled = true;
-            //    },
-            //    canExecute: (string arg) =>
-            //    {
-            //        return !(arg == "." && Entry.Contains("."));
-            //    });
             PickImageCommand = new Command(
                 execute: async () =>
                 {
-                    ImageLoading = true;
+                    ImageWaiting = false;
+
+                    if (!_tesseractApi.Initialized)
+                    {
+                        await _tesseractApi.Init("kor", OcrEngineMode.TesseractOnly);
+                        _tesseractApi.SetPageSegmentationMode(PageSegmentationMode.SparseText);
+                    }
 
                     var imageFile = await TakePic();
                     if (imageFile != null)
                     {
-                        OCRImageSource = ImageSource.FromStream(() => { return imageFile.GetStream(); });
-                    }
+                       
 
-                    ImageLoading = false;
+                        //var imgStream = imageFile.GetStream();
+                        //int imgLen = (int)imgStream.Length;
+                        //var bytes = new byte[imgLen];
+
+                        //imgStream.Position = 0;
+                        //imgStream.Read(bytes, 0, imgLen);
+                        //for (int i = 0; i < imgLen; i++)
+                        //    bytes[i] = (byte)(0xFF ^ bytes[i]);
+
+                        //var memoryStream = new MemoryStream();
+                        //await mediaFile.GetStream().CopyToAsync(memoryStream);
+                        //byte[] imageAsByte = memoryStream.ToArray();
+                        byte[] bytes;
+
+                        using (Stream stream = imageFile.GetStream())
+                        {
+                            //await stream.CopyToAsync(memStream);
+                            //memStream.Seek(0, SeekOrigin.Begin);
+                            var bitmap = SKBitmap.Decode(stream);
+
+                            var img = SKImage.FromBitmap(bitmap);
+                            int height = img.Height;
+                            int width = img.Width;
+
+                            var subset = SKRectI.Create(
+                                (int)(width * 0.2f), (int)(height * 0.4f),
+                                (int)(width * 0.5f), (int)(height * 0.3f));
+                            var placeholder = SKRectI.Empty;
+                            var placeholder2 = SKPointI.Empty;
+
+                            var img2 = img.ApplyImageFilter(
+                                SKImageFilter.CreateColorFilter(_greyFilter),
+                                subset, subset, out placeholder, out placeholder2);
+                            if (img2 == null)
+                            {
+                                Debug.WriteLine("ISNULL");
+                            }
+                            img = img.Subset(subset);
+
+                            bytes = img.Encode().ToArray();
+                            OCRImageSource = ImageSource.FromStream(() =>
+                            {
+                                return new MemoryStream(bytes);
+                            });
+                        }
+                        
+                        // ImageSource.FromStream(() => { return imageFile.GetStream(); });
+                        var tessResult = await _tesseractApi.SetImage(bytes);
+
+                        if (tessResult)
+                        {
+                            Debug.WriteLine("read");
+                            Debug.WriteLine(_tesseractApi.Text);
+                        } else
+                        {
+                            Debug.WriteLine("unavaliable");
+                        }
+                    }
+                    ImageWaiting = true;
+                });
+
+            FoldSelectorCommand = new Command<string>(
+                execute: (string selector) =>
+                {
+                    int i = 0;
+                    switch (selector)
+                    {
+                        case "filter": i = 0; break;
+                        case "tag": i = 1; break;
+                        case "sort": i = 2; break;
+                    }
+                    _isFrameVisible[i] = !_isFrameVisible[i];
+                    OnPropertyChanged("IsFrameVisible");
                 });
         }
         private async Task<MediaFile> TakePic()
@@ -105,9 +157,9 @@ namespace CalKnights
             OnPropertyChanged("YStackValue");
         }
 
-        public bool ImageLoading {
-            private set { SetProperty(ref _imageLoading, value); }
-            get { return _imageLoading; }
+        public bool ImageWaiting {
+            private set { SetProperty(ref _imageWaiting, value); }
+            get { return _imageWaiting; }
         }
 
         public ImageSource OCRImageSource {
@@ -119,7 +171,12 @@ namespace CalKnights
             get { return _dicts; }
         }
 
+        public List<Boolean> IsFrameVisible {
+            get { return _isFrameVisible;  }
+        }
+
         public ICommand PickImageCommand { private set; get; }
+        public ICommand FoldSelectorCommand { private set; get; }
 
         bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
         {
---
+++ CalKnights/CalKnights/Converter/Converter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace CalKnights.Converter
+{
+    class BoolListConverter : IValueConverter
+    { 
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return ((List<Boolean>)value)[ParseInt(parameter)];
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException("ConvertBack BoolListConverter");
+        }
+        int ParseInt(object parameter)
+        {
+            if (parameter is double)
+                return (int)parameter;
+
+            else if (parameter is int)
+                return (int)parameter;
+
+            else if (parameter is string)
+                return int.Parse((string)parameter);
+
+            return 1;
+        }
+    }
+}
